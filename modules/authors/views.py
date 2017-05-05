e9b5e28a59dd4275ac83b68193880436e360cdb6
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-05-04 21:53:40 -0500

    Author API Done

--- modules/authors/views.py
+++ modules/authors/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 from .models import Author
 from .serializers import AuthorSerializer
 # Create your views here.
@@ -13,13 +15,36 @@ class ListAuthor(APIView):
         return Response(serializer.data,status=status.HTTP_200_OK)
 
     def post(self,request):
-        pass
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 class DetailAuthor(APIView):
 
-    def get(self,request):
-        pass
-    def put(self,request):
-        pass
-    def delete(self,request):
-        pass
+    def get(self,request,pk):
+        '''
+        try:
+            author = Author.objects.get(id=pk)
+        except Exception:
+            raise Http404
+        '''
+        author = get_object_or_404(Author,id=pk)
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def put(self,request,pk):
+        author = get_object_or_404(Author,id=pk)
+        serializer = AuthorSerializer(author,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,pk):
+        author = get_object_or_404(Author,id=pk)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

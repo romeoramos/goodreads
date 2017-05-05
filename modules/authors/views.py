0bfd543b22586b1f07373627e6284e33cea9709d
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-05-04 20:39:04 -0500

    Starting Author serializers

--- modules/authors/views.py
+++ modules/authors/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Author
 # Create your views here.
+
+class ListAuthor(APIView):
+
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+class DetailAuthor(APIView):
+
+    def get(self,request):
+        pass
+    def put(self,request):
+        pass
+    def delete(self,request):
+        pass
